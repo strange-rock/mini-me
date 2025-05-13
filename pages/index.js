a7acb1502bedc5e7f0ef27ec698cc6ddf7d48235
Uttkarsh M <77172864+strange-rock@users.noreply.github.com>
2025-05-13 16:43:57 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -360,10 +360,17 @@ export default function AgentComponent() {
           </div>
         ))}
         {isTyping && (
-          <div className="typing-indicator">
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
+          <div
+            style={{
+              ...bubbleStyles.agent,
+              animationDelay: '0s',
+            }}
+          >
+            <div className="typing-indicator" style={{ padding: 0 }}>
+              <div className="dot" style={{ background: '#393836' }}></div>
+              <div className="dot" style={{ background: '#393836' }}></div>
+              <div className="dot" style={{ background: '#393836' }}></div>
+            </div>
           </div>
         )}
         <div ref={messagesEndRef} />
@@ -488,33 +495,26 @@ export default function AgentComponent() {
             alignItems: "center",
             justifyContent: "center",
             cursor: isLoading ? "default" : "pointer",
-            transition: "transform 0.2s ease",
-            transform: isSubmitHovered ? "scale(1.05)" : "scale(1)",
+            transition: "transform 0.2s ease, opacity 0.2s ease",
+            transform: isSubmitHovered && !isLoading ? "scale(1.05)" : "scale(1)",
+            opacity: isLoading ? 0.5 : 1,
             padding: 0,
             flexShrink: 0,
             boxSizing: "border-box",
           }}
-          onMouseOver={() => setIsSubmitHovered(true)}
+          onMouseOver={() => !isLoading && setIsSubmitHovered(true)}
           onMouseOut={() => setIsSubmitHovered(false)}
         >
-          {!isLoading ? (
-            <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ width: '24px', height: '24px' }}>
-              <g clipPath="url(#clip0_4086_8473)">
-                <path fillRule="evenodd" clipRule="evenodd" d="M1.84647 7.15123C1.54566 7.21608 1.31498 7.45811 1.26464 7.7617C1.2143 8.06528 1.35452 8.36881 1.6183 8.52729L8.13474 12.4421L14.3544 8.08705C14.6938 7.84947 15.1614 7.93193 15.399 8.27123C15.6366 8.61054 15.5541 9.0782 15.2148 9.31578L8.99537 13.6707L10.4455 21.1339C10.5042 21.436 10.7415 21.6715 11.044 21.7281C11.3465 21.7846 11.6528 21.6506 11.8166 21.3901L22.7919 3.93893C22.9526 3.68349 22.9445 3.35665 22.7714 3.10947C22.5983 2.86228 22.294 2.7429 21.999 2.80649L1.84647 7.15123Z" fill="#F1EFF0"/>
-              </g>
-              <defs>
-                <clipPath id="clip0_4086_8473">
-                  <rect width="24" height="24" />
-                </clipPath>
-              </defs>
-            </svg>
-          ) : (
-            <div className="typing-indicator">
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-            </div>
-          )}
+          <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ width: '24px', height: '24px' }}>
+            <g clipPath="url(#clip0_4086_8473)">
+              <path fillRule="evenodd" clipRule="evenodd" d="M1.84647 7.15123C1.54566 7.21608 1.31498 7.45811 1.26464 7.7617C1.2143 8.06528 1.35452 8.36881 1.6183 8.52729L8.13474 12.4421L14.3544 8.08705C14.6938 7.84947 15.1614 7.93193 15.399 8.27123C15.6366 8.61054 15.5541 9.0782 15.2148 9.31578L8.99537 13.6707L10.4455 21.1339C10.5042 21.436 10.7415 21.6715 11.044 21.7281C11.3465 21.7846 11.6528 21.6506 11.8166 21.3901L22.7919 3.93893C22.9526 3.68349 22.9445 3.35665 22.7714 3.10947C22.5983 2.86228 22.294 2.7429 21.999 2.80649L1.84647 7.15123Z" fill="#F1EFF0"/>
+            </g>
+            <defs>
+              <clipPath id="clip0_4086_8473">
+                <rect width="24" height="24" />
+              </clipPath>
+            </defs>
+          </svg>
         </button>
       </div>
 
