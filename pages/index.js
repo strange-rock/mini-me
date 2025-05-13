3643dced7b296747d13f079127ed7aa9dd8fa011
Uttkarsh M <77172864+strange-rock@users.noreply.github.com>
2025-05-13 16:37:01 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -493,11 +493,11 @@ export default function AgentComponent() {
         >
           {!isLoading ? (
             <svg
-              width="24"
-              height="24"
+              width="100%"
+              height="100%"
               viewBox="0 0 24 24"
-              fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              style={{ width: '24px', height: '24px' }}
             >
               <path
                 d="m21.426 11.095-17-8A1 1 0 0 0 3.03 4.242l1.212 4.849L12 12l-7.758 2.909-1.212 4.849a.998.998 0 0 0 1.396 1.147l17-8a1 1 0 0 0 0-1.81z"
