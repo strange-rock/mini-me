3859cebcabda29977d6ef2169ed8ae7f85c01a86
Uttkarsh M <77172864+strange-rock@users.noreply.github.com>
2025-05-13 16:41:11 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -498,17 +498,15 @@ export default function AgentComponent() {
           onMouseOut={() => setIsSubmitHovered(false)}
         >
           {!isLoading ? (
-            <svg
-              width="100%"
-              height="100%"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              style={{ width: '24px', height: '24px' }}
-            >
-              <path
-                d="m21.426 11.095-17-8A1 1 0 0 0 3.03 4.242l1.212 4.849L12 12l-7.758 2.909-1.212 4.849a.998.998 0 0 0 1.396 1.147l17-8a1 1 0 0 0 0-1.81z"
-                fill="#F1EFF0"
-              />
+            <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ width: '24px', height: '24px' }}>
+              <g clipPath="url(#clip0_4086_8473)">
+                <path fillRule="evenodd" clipRule="evenodd" d="M1.84647 7.15123C1.54566 7.21608 1.31498 7.45811 1.26464 7.7617C1.2143 8.06528 1.35452 8.36881 1.6183 8.52729L8.13474 12.4421L14.3544 8.08705C14.6938 7.84947 15.1614 7.93193 15.399 8.27123C15.6366 8.61054 15.5541 9.0782 15.2148 9.31578L8.99537 13.6707L10.4455 21.1339C10.5042 21.436 10.7415 21.6715 11.044 21.7281C11.3465 21.7846 11.6528 21.6506 11.8166 21.3901L22.7919 3.93893C22.9526 3.68349 22.9445 3.35665 22.7714 3.10947C22.5983 2.86228 22.294 2.7429 21.999 2.80649L1.84647 7.15123Z" fill="#F1EFF0"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_4086_8473">
+                  <rect width="24" height="24" />
+                </clipPath>
+              </defs>
             </svg>
           ) : (
             <div className="typing-indicator">
